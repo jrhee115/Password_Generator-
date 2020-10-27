448390e99878b173f120e6a02629b0021761e0e4
Justin Rhee <jayrhee0514@gmail.com>
2020-10-26 22:14:24 -0400

    added a concat to combine all arrays, then tried to make a for statement so randomize the combined arrays

--- Develop/script.js
+++ Develop/script.js
@@ -21,17 +21,32 @@ generateBtn.addEventListener("click", writePassword);
 
 //Password possible characters
 var symbolChar = ["!", "@", "#", "$", "%", "^", "&", "*", "+", "-", "?", "<", ">", "~"];
-var numberChar= [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var numberChar= ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 var lowerChar= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperChar= ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var finalPassword = "";
 
-
+//Need to get the password between 8 and 128 characters
   var passChar = prompt("How many characters would you like your password to contain");
 
   if (passChar < 8) {
     alert ("Needs between 8 - 128 characters");
   } 
-  else if (passCharacter > 128) {
+  if (passChar > 128) {
     alert ("Needs between 8 - 128 characters");
   }
+// // concat array combines all 3 arrays
+// var allChar = symbolChar.concat (numberChar, lowerChar, upperChar);
+// console.log(allChar);
+
+function randomChar(allChar) {
+
+var allChar = symbolChar.concat (numberChar, lowerChar, upperChar);
+  for (var i = 0; i < allchar.length; i++) {
+    var i = Math.floor(Math.random() * (i + 1));
+    var temp = allChar[i]
+    allChar[i] = allChar [j]
+    allChar [j] + temp;
+  }
+}
+console.log(allChar);
