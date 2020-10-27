7f418dce50444ac5bf5aca10ff42bfdb7f1ad307
Justin Rhee <jayrhee0514@gmail.com>
2020-10-27 15:46:26 -0400

    new approach added a loop and randomization method

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,17 @@
+//Variables
+var passChar
+var passSymbolChar
+var passLowerChar
+var passUpperChar
+var passNumberChar
+
+//Password possible characters
+var symbolChar = ["!", "@", "#", "$", "%", "^", "&", "*", "+", "-", "?", "<", ">", "~"];
+var numberChar= ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var lowerChar= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperChar= ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var finalPassword = " ";
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -7,46 +21,23 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-  var passChar = prompt("How many characters would you like your password to contain");
-  var passSymbolChar = confirm("Click OK to confirm including specialized chatacters");
-  var passLowerChar = confirm("Click OK to confirm including lowercase chatacters");
-  var passUpperChar = confirm("Click OK to confirm including uppercase chatacters");
-  var passNumnerChar= confirm("Click OK to confirm including number chatacters");
-
-//Password possible characters
-var symbolChar = ["!", "@", "#", "$", "%", "^", "&", "*", "+", "-", "?", "<", ">", "~"];
-var numberChar= ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-var lowerChar= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upperChar= ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var finalPassword = "";
-
 //Need to get the password between 8 and 128 characters
-  var passChar = prompt("How many characters would you like your password to contain");
+var passChar = prompt("How many characters would you like your password to contain");
 
-  if (passChar < 8) {
-    alert ("Needs between 8 - 128 characters");
+//testing prompt
+// alert (" code is " + passChar)
+
+if (passChar < 8 || passChar > 128) {
+  alert ("Needs between 8 - 128 characters");
+} else {
+
+  passSymbolChar = confirm("Click OK to confirm including specialized chatacters");
+  passLowerChar = confirm("Click OK to confirm including lowercase chatacters");
+  passUpperChar = confirm("Click OK to confirm including uppercase chatacters");
+  passNumberChar = confirm("Click OK to confirm including number chatacters");
   } 
-  if (passChar > 128) {
-    alert ("Needs between 8 - 128 characters");
-  }
-// // concat array combines all 3 arrays
-// var allChar = symbolChar.concat (numberChar, lowerChar, upperChar);
-// console.log(allChar);
-
-function randomChar(allChar) {
-
-var allChar = symbolChar.concat (numberChar, lowerChar, upperChar);
-  for (var i = 0; i < allchar.length; i++) {
-    var i = Math.floor(Math.random() * (i + 1));
-    var temp = allChar[i]
-    allChar[i] = allChar [j]
-    allChar [j] + temp;
-  }
-}
-console.log(allChar);
